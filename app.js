f0085767250804aeeeb7737fa69dd080bad25ec5
MistahSanD <wadpey@gmail.com>
2018-11-28 01:23:39 -0800

    data not grabbed

---
+++ app.js
@@ -0,0 +1,112 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyArNHW7CMDpRcLOMr-UMHODe6fZ5m1fZKs",
+    authDomain: "train-timer-eaa94.firebaseapp.com",
+    databaseURL: "https://train-timer-eaa94.firebaseio.com",
+    projectId: "train-timer-eaa94",
+    storageBucket: "train-timer-eaa94.appspot.com",
+    messagingSenderId: "931545718416"
+  };
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+
+  var tMinutesTillTrain = 0;
+
+  //Show and update current time. Use setInterval method to update time.
+  function displayRealTime() {
+  setInterval(function(){
+      $('#current-time').html(moment().format('hh:mm:ss A'))
+    }, 1000);
+  }
+  displayRealTime();
+
+// button for adding train
+$(".btn btn-primary").on("click", function(event) {
+    event.preventDefault();
+
+    // grab the user input
+    var trainName = $("#trainName").val().trim();
+    var sendLocation = $("#userDestination").val().trim();
+    var firstSend = $("#1stTrain").val().trim();
+    var trainInterval = $("#frequent").val().trim();
+
+
+    // temporary data holder
+    var newTrain = {
+        name: trainName,
+        destination: sendLocation,
+        start: firstSend,
+        freq: trainInterval
+    };
+
+// upload the train data to database:
+    database.ref().push(newTrain);
+
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.start);
+    console.log(newTrain.freq);
+
+    alert("Train has successfully been added!");
+
+    // clear text boxes
+    $("#trainName").val("");
+    $("#userDestination").val("");
+    $("#1stTrain").val("");
+    $("#frequent").val("");
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+
+    var trainName = childSnapshot.val().name;
+    var sendLocation = childSnapshot.val().destination;
+    var firstSend = childSnapshot.val().start;
+    var trainInterval = childSnapshot.val().freq;
+
+    console.log(trainName);
+    console.log(sendLocation);
+    console.log(firstSend);
+    console.log(trainInterval);
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstSend, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % trainInterval;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var minutesAway = trainInterval - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + minutesAway);
+
+    // Next Train
+    var upcomingTrain = moment().add(minutesAway, "minutes");
+    console.log("ARRIVAL TIME: " + moment(upcomingTrain).format("hh:mm"));
+
+
+    // create the table based on entry
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(sendLocation),
+        $("<td>").text(trainInterval),
+        $("<td>").text(nextArrive),
+        $("<td>").text(minAway)
+    );
+
+    // append the new row to the table 
+    $("#train-table").append(newRow);
+
+
+});
